5cd726f74c9831d4b62d30aad195faa59f6eb38d
stereo3d <uwe2@gmx.net>
2025-05-17 22:47:04 +0900

    Update svs.rb
    edit sha

--- svs.rb
+++ svs.rb
@@ -2,7 +2,7 @@ class Svs < Formula
   desc "Stereoscopic video splitter command-line tool"
   homepage "https://github.com/stereo3d/svs"
   url "https://github.com/stereo3d/svs/releases/download/v1.0.0/svs.tar.gz"
-  sha256 "YOUR_GENERATED_SHA256"
+  sha256 "a969925560453fbd03c50abecc0119fdbededae344d9f4f2e344e331d91a4957"
   version "1.0.0"
 
   def install
